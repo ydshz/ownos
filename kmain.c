edf347d9c6697900f771881bf7e4a47dcda41cb9
Yannis Storrer <yannis.storrer@web.de>
2021-02-25 12:47:01 +0100

    Added write function

--- kmain.c
+++ kmain.c
@@ -1,3 +1,21 @@
-void kmain(){
-  return;
+char *fb = (char *) 0x000B8000;
+
+/** fb_write_cell:
+*  Writes a character with the given foreground and background to position i
+*  in the framebuffer.
+*
+*  @param i  The location in the framebuffer
+*  @param c  The character
+*  @param fg The foreground color
+*  @param bg The background color
+*/
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
+{
+  fb[i] = c;
+  fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+}
+
+void kmain()
+{
+  fb_write_cell(0, 'A', 2, 8);
 }
