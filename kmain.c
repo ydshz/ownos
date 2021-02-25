b1e18df25c6400faf1821ef4f18dc109fccde292
Yannis Storrer <yannis.storrer@web.de>
2021-02-25 13:01:35 +0100

    Moved input stuff into stdio

--- kmain.c
+++ kmain.c
@@ -1,19 +1,4 @@
-char *fb = (char *) 0x000B8000;
-
-/** fb_write_cell:
-*  Writes a character with the given foreground and background to position i
-*  in the framebuffer.
-*
-*  @param i  The location in the framebuffer
-*  @param c  The character
-*  @param fg The foreground color
-*  @param bg The background color
-*/
-void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
-{
-  fb[i] = c;
-  fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
-}
+#include "stdio.h"
 
 void kmain()
 {
---
+++ stdio.h
@@ -0,0 +1,6 @@
+#ifndef LOG_H
+#define LOG_H
+
+void fb_write_cell(unsigned int i,char c, unsigned char fg, unsigned char bg);
+
+#endif
