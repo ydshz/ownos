461b41d620b5ba3e81d14bc5db65e67907d1a49a
Yannis Storrer <yannis.storrer@web.de>
2021-02-27 00:07:17 +0100

    Rewrote and commented the assembly part

--- loader.s
+++ loader.s
@@ -1,31 +1,43 @@
 global loader
 
+;Declare the kmain function as extern
 extern kmain
 
+;Setup multiboot header constants
 MAGIC_NUMBER equ 0x1BADB002
 FLAGS        equ 0x0
 CHECKSUM     equ -MAGIC_NUMBER
- KERNEL_STACK_SIZE equ 4096
 
-section  .bss
-align 4
-kernel_stack:
-  resb KERNEL_STACK_SIZE
+;Define the stack size of the kernel
+KERNEL_STACK_SIZE equ 4096
 
-section .text:
+;Place the multiboot header in first section
+section .multiboot
 align 4
   dd MAGIC_NUMBER
   dd FLAGS
   dd CHECKSUM
 
+;Allocate memory for the stack
+section  .bss
+align 4
+  resb KERNEL_STACK_SIZE
+
+;Here goes the code
+section .text:
+align 4
+
+;Entrypoint of the kernel
 loader:
-  mov eax, 0xCAFEBABE
+    ;Set the stack pointer on top of the stack
+    mov esp, loader+KERNEL_STACK_SIZE
+    ;Call the extern C function
+    call kmain
 
-stack_setup:
-   mov esp, kernel_stack+KERNEL_STACK_SIZE
-
-enter_kmain:
-  call kmain
-
+;Infinite loop
 .loop:
-  jmp .loop
+    ;Disable all interupts
+    cli
+    ;Wait infinite for the next interupt
+    hlt
+    jmp .loop
